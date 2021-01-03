591620ef963cdf520fa3dbc277d84c61a8114bee
Kwakcena <rhkrgudwh@gmail.com>
2021-01-03 17:21:05 +0900

    Fail] python의 zip과 같이 했지만 시간초과 발생
    * python 풀이를 참고했는데 오히려 모든 효율성에서 시간초과가 났다.

--- 20210103/IHateSameNumber.test.js
+++ 20210103/IHateSameNumber.test.js
@@ -1,14 +1,14 @@
 const solution = (arr) => {
-  const stack = [];
-  stack.push(arr[0]);
-
-  for(let i = 1; i < arr.length; i++) {
-    if(stack[stack.length - 1] !== arr[i]) {
-      stack.push(arr[i]);
+  return zip(arr, arr.slice(1)).reduce((answer, [first, second]) => {
+    if(first !== second) {
+      answer.push(first);
     }
-  }
+    return answer;
+  }, []);
+}
 
-  return stack;
+const zip = (a, b) => {
+  return a.map((number, index) => [number, b[index]]);
 }
 
 test('solution', () => {
@@ -16,4 +16,11 @@ test('solution', () => {
   expect(solution([4, 4, 4, 3, 3])).toEqual([4, 3]);
   expect(solution([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
   expect(solution([0])).toEqual([0]);
+})
+
+test('zip two arrays', () => {
+  expect(zip([1, 2, 3], [2, 3, 4])).toEqual([[1, 2], [2, 3], [3, 4]]);
+  expect(zip([1, 1, 3, 3, 0, 1, 1], [1, 3, 3, 0, 1, 1, ''])).toEqual([
+    [1, 1], [1, 3], [3, 3], [3, 0], [0, 1], [1, 1], [1, ''],
+  ]);
 })
