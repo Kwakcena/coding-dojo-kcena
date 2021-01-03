321551da98c38e00db7ac6924726072573915433
Kwakcena <rhkrgudwh@gmail.com>
2021-01-03 17:24:19 +0900

    Pass] for문을 이용한 풀이
    * i와 j 인덱스를 두고 for문으로 풀었다.
    * 효율성 검사가 거의 14.50ms, 76MB 대로 풀렸다.

--- 20210103/IHateSameNumber.test.js
+++ 20210103/IHateSameNumber.test.js
@@ -1,14 +1,11 @@
 const solution = (arr) => {
-  return zip(arr, arr.slice(1)).reduce((answer, [first, second]) => {
-    if(first !== second) {
-      answer.push(first);
+  const answer = [];
+  for(let i = 0, j = 1; i < arr.length; i++, j++) {
+    if(arr[i] !== arr[j]) {
+      answer.push(arr[i]);
     }
-    return answer;
-  }, []);
-}
-
-const zip = (a, b) => {
-  return a.map((number, index) => [number, b[index]]);
+  }
+  return answer;
 }
 
 test('solution', () => {
@@ -16,11 +13,4 @@ test('solution', () => {
   expect(solution([4, 4, 4, 3, 3])).toEqual([4, 3]);
   expect(solution([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
   expect(solution([0])).toEqual([0]);
-})
-
-test('zip two arrays', () => {
-  expect(zip([1, 2, 3], [2, 3, 4])).toEqual([[1, 2], [2, 3], [3, 4]]);
-  expect(zip([1, 1, 3, 3, 0, 1, 1], [1, 3, 3, 0, 1, 1, ''])).toEqual([
-    [1, 1], [1, 3], [3, 3], [3, 0], [0, 1], [1, 1], [1, ''],
-  ]);
 })
