4bc3cdf406b5b6e5afc9b8d55898963afdb365d7
Kwakcena <rhkrgudwh@gmail.com>
2021-02-03 21:30:44 +0900

    Pass] 홀수, 짝수 모두 고려해보기
    하나의 string 에 대해서 펠린드롬의 길이가 홀수일 경우 기준 index가
    i값이고, 짝수일 경우 left는 i, right는 i+1을 주어서 펠린드롬의 길이가
    홀수개일 수도 있고 짝수개의 길이일 수도 있음을 고려한다.

--- 20210203/pailndrome.test.js
+++ 20210203/pailndrome.test.js
@@ -1,28 +1,40 @@
-const solution = (s) => {
-  let answer = 1;
-  for (let i = 0; i < s.length; i++) {
-    let j = 1;
-    let temp = 1;
-    while (i - j >= 0 && i + j < s.length && s[i - j] === s[i + j]) {
-      temp += 2;
-      j++;
-    }
-    if (temp > answer) {
-      answer = temp;
-    }
+const pailndrome = (s, l, r) => {
+  while (l >= 0 && r < s.length && s[l] === s[r]) {
+    l--;
+    r++;
   }
-  return answer;
+  return r - l - 1;
 };
 
-test('solution', () => {
+const solution = (s) => {
+  return s
+    .split('')
+    .reduce(
+      (answer, _, i) =>
+        Math.max(
+          answer,
+          Math.max(pailndrome(s, i, i), pailndrome(s, i, i + 1))
+        ),
+      0
+    );
+};
+
+test('solution 문자열의 길이가 홀수인 경우', () => {
+  expect(solution('abcba')).toBe(5);
   expect(solution('abcdcba')).toBe(7);
   expect(solution('abacde')).toBe(3);
   expect(solution('abcabcdcbae')).toBe(7);
-  expect(solution('aaaa')).toBe(4);
   expect(solution('abcde')).toBe(1);
   expect(solution('a')).toBe(1);
   expect(solution('abcbaqwertrewqq')).toBe(9);
   expect(solution('abcbaqwqabcba')).toBe(13);
-  expect(solution('abba')).toBe(4);
   expect(solution('abaabaaaaaaa')).toBe(7);
+});
+
+test('solution 문자열의 길이가 짝수인 경우', () => {
+  expect(solution('aaaa')).toBe(4);
+  expect(solution('aaab')).toBe(3);
+  expect(solution('aaba')).toBe(3);
+  expect(solution('abba')).toBe(4);
+  expect(solution('bcba')).toBe(3);
 });
