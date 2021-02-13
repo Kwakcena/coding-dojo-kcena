e0c0dbe77220ec150cfa38a67c61c7ab921f255b
Kwakcena <rhkrgudwh@gmail.com>
2021-02-13 14:54:10 +0900

    Fail] 꼬리재귀 적용해도 런타임 에러
    꼬리재귀를 적용해도 런타임 에러가 발생했다.
    
    반복문을 이용해서 풀어야 할 것 같다.

--- 20210213/fibonacci_number.test.js
+++ 20210213/fibonacci_number.test.js
@@ -1,18 +1,14 @@
 // F(n) = F(n-1) + F(n-2)
 const solution = (n) => {
-  const memo = [0, 1];
-  const fibonacci = (number) => {
-    if (number <= 1) return number;
-    if (!memo[number]) {
-      memo[number] = fibonacci(number - 1) + fibonacci(number - 2);
-    }
-    return memo[number] % 1234567;
+  const fibonacci = (number, before, next) => {
+    if (number === 0) return before;
+    return fibonacci(number - 1, next, (before + next) % 1234567);
   };
-  return fibonacci(n);
+  return fibonacci(n, 0, 1);
 };
 
 test('solution', () => {
-  // expect(solution(2)).toBe(1);
-  // expect(solution(3)).toBe(2);
+  expect(solution(2)).toBe(1);
+  expect(solution(3)).toBe(2);
   expect(solution(100000)).toBe(5);
 });
