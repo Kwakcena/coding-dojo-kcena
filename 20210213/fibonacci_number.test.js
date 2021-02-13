461bd0a19680081ff90594ae64b38cba783da70e
Kwakcena <rhkrgudwh@gmail.com>
2021-02-13 14:58:24 +0900

    Pass] 반복문 이용
    꼬리재귀를 반복문으로 풀어서 구현하여 문제를 해결했다.

--- 20210213/fibonacci_number.test.js
+++ 20210213/fibonacci_number.test.js
@@ -1,14 +1,17 @@
 // F(n) = F(n-1) + F(n-2)
 const solution = (n) => {
-  const fibonacci = (number, before, next) => {
-    if (number === 0) return before;
-    return fibonacci(number - 1, next, (before + next) % 1234567);
-  };
-  return fibonacci(n, 0, 1);
+  let before = 0;
+  let next = 1;
+  for (; n > 0; n--) {
+    let temp = before;
+    before = next;
+    next = (temp + next) % 1234567;
+  }
+  return before;
 };
 
 test('solution', () => {
   expect(solution(2)).toBe(1);
   expect(solution(3)).toBe(2);
-  expect(solution(100000)).toBe(5);
+  expect(solution(5)).toBe(5);
 });
